17214aca2b12ee6295f2ea3c4b0eff26f05f9558
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-02 18:09:56 +0200

    initial login connected to BE

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    const url = "http://localhost:5000/tokens";
+
+    const payload = { username, password };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="password">username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="password">password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
