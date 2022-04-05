bd6b6b9891d383e8ed294fc17665c156107f9872
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-05 23:28:50 +0200

    changed spelling

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.ObjectId, ref: "User" },
   content: { type: String, required: true, maxlength: 100 },
   date: { type: Date, default: Date.now },
   done: { type: Boolean, default: false },
 });
 
-const Todo = mongoose.model("Todo", todoSchema);
+const ToDo = mongoose.model("ToDo", todoSchema);
 
-exports.Todo = Todo;
+exports.ToDo = ToDo;
