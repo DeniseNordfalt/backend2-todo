ce91ee7c0ac9a5aaf0a2c2950be4f67505ed7941
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-05 23:33:23 +0200

    added some routes and a temporary nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 import UserCreatePage from "./pages/UserCreatePage";
 import LoginPage from "./pages/LoginPage";
+import ToDoPage from "./pages/ToDoPage";
+import TodoCreatePage from "./pages/TodoCreatePage";
 
 import "./App.css";
 
 function App() {
   return (
     <>
+      {/* temp nav */}
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/todo">Todo</Link>
+        </li>
+        <li>
+          <Link to="/create-todo">Create Todo</Link>
+        </li>
+      </ul>
       <Routes>
         <Route path="/register" element={<UserCreatePage />} />
         <Route path="/login" element={<LoginPage />} />
+        {/* TEMP ROUTES */}
+        <Route path="/todo" element={<ToDoPage />} />
+        <Route path="/create-todo" element={<TodoCreatePage />} />
       </Routes>
     </>
   );
