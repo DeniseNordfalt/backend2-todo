0b8f8ad8fdc158e8c99887ce28219a0ca45cee65
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-24 00:39:17 +0200

    globalstyles

---
+++ client/src/globalstyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Globalstyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+
+}
+
+body{
+    background-color: lightskyblue;
+}
+
+
+`;
+
+export default Globalstyle;
