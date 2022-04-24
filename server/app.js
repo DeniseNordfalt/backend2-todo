8f55abc7224d1ea2a6a91e55df6dfcbc8afb3c73
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-24 21:38:53 +0200

    app changes

--- server/app.js
+++ server/app.js
@@ -3,25 +3,31 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const multer = require("multer");
+const morgan = require("morgan");
+// const { requireLogin } = require("./controllers/auth");
 
 const { User } = require("./models/user");
 const { ToDo } = require("./models/todo");
+const { sendStatus } = require("express/lib/response");
 
 const app = express();
 
-//variables - move to .env?
-const PORT = 5000;
-const JWT_SECRET = "supersecret";
+//variables
+const PORT = process.env.PORT;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 //checks headers for token
 app.use((req, res, next) => {
   const authHeader = req.header("Authorization");
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    console.log("token: ", token);
+    console.log("token", token);
     req.user = jwt.verify(token, JWT_SECRET);
   }
   next();
@@ -36,13 +42,58 @@ const requireLogin = (req, res, next) => {
   }
 };
 
-//creates user
-app.post("/users", async (req, res) => {
-  const { username, password } = req.body;
-  const user = new User({ username, password });
-  await user.save();
-  res.json({ username });
+app.get("/", async (req, res) => {
+  res.send("hello");
 });
+///////////////////////////////////////////////////////
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.use(upload.array("files"));
+app.use("/uploads", express.static("./uploads"));
+
+app.post("/todos", requireLogin, async (req, res) => {
+  console.log("req.body", req.body);
+  console.log("files", req.files);
+  const { title, content } = req.body;
+  const files = req.files;
+  const user = req.user;
+  if (title) {
+    const todo = new ToDo({ title, content, files, author: user.userId });
+    await todo.save();
+
+    res.json({ user, todo });
+  } else {
+    res.status(400).json("Title is required");
+  }
+});
+
+// app.get("/todos/:id/:file", requireLogin, async (req, res) => {
+//   console.log("params", req.params);
+//   console.log("query", req.query);
+//   const params = req.params;
+//   const todo = await ToDo.findOne({ _id: params.id });
+//   await ToDo.findOneAndUpdate(
+//     {
+//       _id: params.id,
+//       files: { $elemMatch: { filename: params.file } },
+//     },
+//     {
+//       $pull: { files: { filename: params.file } },
+//     }
+//   );
+
+//   res.send(todo.files[params.file]);
+// });
+////////////////////////////////////////////////////
 
 //gets token
 app.post("/tokens", async (req, res) => {
@@ -51,47 +102,17 @@ app.post("/tokens", async (req, res) => {
   if (user) {
     const userId = user._id.toString();
     const token = jwt.sign({ userId, username: user.username }, JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "24h",
       subject: userId,
     });
-    res.json({ token });
+    res.json({ token, userId, username: user.username });
   } else {
     res.sendStatus(401);
   }
 });
 
-//creates a todo-item
-// app.post("/todos", async (req, res) => {
-//   const { content } = req.body;
-//   const todo = new ToDo({ content });
-//   await todo.save();
-//   res.json({ content });
-// });
-
-//creates a todo-item for a specific user
-app.post("/todos", async (req, res) => {
-  const { content } = req.body;
-  const user = req.user;
-  const todo = new ToDo({ content, user: user._id });
-  await todo.save();
-
-  res.json({ user, todo });
-});
-
-//lists all todos
-app.get("/todos", async (req, res) => {
-  const todos = await ToDo.find().sort({ date: -1 }).exec();
-
-  res.json({ todos });
-});
-
-//list all todos connected to a user
-app.get("/todos", requireLogin, async (req, res) => {
-  const todos = await ToDo.find({ user: req.user._id })
-    .sort({ date: -1 })
-    .exec();
-  res.json({ todos });
-});
+app.use("/users", require("./routes/users"));
+app.use("/todos", require("./routes/todos"));
 
 mongoose.connect("mongodb://127.0.0.1/todo");
 
