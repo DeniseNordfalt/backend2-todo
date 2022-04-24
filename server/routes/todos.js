a4d1a4cf80814ca76522c0aeb2369c0bbbfafb21
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-24 21:38:01 +0200

    todo routes

---
+++ server/routes/todos.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+
+const { User } = require("../models/user");
+const { ToDo } = require("../models/todo");
+
+//GET ALL TODOS
+router.get("/", async (req, res) => {
+  const user = req.user;
+  if (user) {
+    console.log("USER", req.user);
+    const todos = await ToDo.find({ author: req.user.userId, done: false })
+      .sort({ date: -1 })
+      .exec();
+    res.json({ todos });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+//CREATE A TODO
+// router.post("/", async (req, res) => {
+//   console.log("req.body", req.body);
+//   console.log("files", req.files);
+//   const { title, content, files } = req.body;
+//   const user = req.user;
+//   if (title) {
+//     const todo = new ToDo({ title, content, files, author: user.userId });
+//     await todo.save();
+
+//     res.json({ user, todo });
+//   } else {
+//     res.status(400).json("Title is required");
+//   }
+// });
+
+//GET COMPLETED
+router.get("/completed", async (req, res) => {
+  console.log("user", req.user);
+  const user = req.user;
+  //find out why this is undefined
+  console.log("userid", req.userId);
+  if (user) {
+    const todos = await ToDo.find({ author: req.user.userId, done: true })
+      .sort({ date: -1 })
+      .exec();
+    return res.json({ todos });
+  } else {
+    return res.status(401).json("You have to be logged in to see todos");
+  }
+});
+
+//GET TODO BY ID TOGGLE
+router.get("/:id", async (req, res) => {
+  const todo = await ToDo.findOne({ _id: req.params.id });
+  todo.done = !todo.done;
+  await todo.save();
+
+  res.json(todo);
+});
+
+router.get("/todos/:id/:file", async (req, res) => {
+  console.log("params", req.params);
+  console.log("query", req.query);
+  const params = req.params;
+  const todo = await ToDo.findOne({ _id: params.id });
+  await ToDo.findOneAndUpdate(
+    {
+      _id: params.id,
+      files: { $elemMatch: { filename: params.file } },
+    },
+    {
+      $pull: { files: { filename: params.file } },
+    }
+  );
+
+  res.send(todo.files[params.file]);
+});
+
+//DELETE TODO BY ID
+router.delete("/:id", async (req, res) => {
+  const todo = await ToDo.findOneAndDelete({ _id: req.params.id });
+  res.json(todo);
+});
+
+// //update a todo
+router.put("/:id", async (req, res) => {
+  const { title, content, files } = req.body;
+  const todo = await ToDo.findOneAndUpdate(
+    { _id: req.params.id },
+    { title, content, files },
+    { new: true }
+  );
+  res.json({ todo });
+});
+
+module.exports = router;
