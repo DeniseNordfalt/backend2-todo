fa1b8b35f60bf728e94561862ad3e02b55a74938
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-24 21:33:42 +0200

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,38 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 
-import UserCreatePage from "./pages/UserCreatePage";
+import UserCreatePage from "./pages/user/UserCreatePage";
 import LoginPage from "./pages/LoginPage";
-import ToDoPage from "./pages/ToDoPage";
-import TodoCreatePage from "./pages/TodoCreatePage";
+import ToDoPage from "./pages/todo/ToDoPage";
+import TodoCreatePage from "./pages/todo/TodoCreatePage";
+import TodoCompletedPage from "./pages/todo/TodoCompletedPage";
+import TodoUncompletedPage from "./pages/todo/TodoUncompletedPage";
 
 import "./App.css";
+import Globalstyle from "./globalstyles";
+
+const UserContext = createContext({});
 
 function App() {
+  const [userContext, setUserContext] = useState(null);
   return (
     <>
-      {/* temp nav */}
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/todo">Todo</Link>
-        </li>
-        <li>
-          <Link to="/create-todo">Create Todo</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/register" element={<UserCreatePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        {/* TEMP ROUTES */}
-        <Route path="/todo" element={<ToDoPage />} />
-        <Route path="/create-todo" element={<TodoCreatePage />} />
-      </Routes>
+      <UserContext.Provider value={{ userContext, setUserContext }}>
+        <Globalstyle />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<UserCreatePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* TEMP ROUTES */}
+          <Route path="/todo/" element={<ToDoPage />} />
+          <Route path="/todo/create-todo" element={<TodoCreatePage />} />
+          <Route path="/todo/completed" element={<TodoCompletedPage />} />
+          <Route path="/todo/uncompleted" element={<TodoUncompletedPage />} />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
 
+export { UserContext };
 export default App;
